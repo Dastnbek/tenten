9918ff65fd8820ca9334a3650c8d2a01565746d5
Sirojiddin Sobirov <dostonbek.oripjonov@mail.ru>
2024-06-27 16:45:42 +0500

    Added speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "@radix-ui/themes";
 import MainContainer from "./MainContainer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = () => {
   return (
     <Container>
       <MainContainer />
+      <SpeedInsights />
     </Container>
   );
 };
