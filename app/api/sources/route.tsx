05cb94b1cb6f480231ef22ee069990cd6ca1f07f
Sirojiddin Sobirov <dostonbek.oripjonov@mail.ru>
2024-06-27 14:06:12 +0500

    Check only serper

--- app/api/sources/route.tsx
+++ app/api/sources/route.tsx
@@ -42,73 +42,73 @@ export async function POST(request: NextRequest) {
   const { query } = await request.json();
   const sourceCount = 5;
   try {
-    const sourceQuery = `https://www.google.com/search?q=${query}`;
     await useSerperAPI(query);
-    const responseFromSources = await fetch(sourceQuery);
-    const html = await responseFromSources.text();
-    console.log("resposne", responseFromSources);
+    // const sourceQuery = `https://www.google.com/search?q=${query}`;
+    // const responseFromSources = await fetch(sourceQuery);
+    // const html = await responseFromSources.text();
+    // console.log("resposne", responseFromSources);
 
-    const $ = cheerio.load(html);
-    const linkTags = $("a");
-    let links: string[] = [];
+    // const $ = cheerio.load(html);
+    // const linkTags = $("a");
+    // let links: string[] = [];
 
-    linkTags.each((i, link) => {
-      const href = $(link).attr("href");
+    // linkTags.each((i, link) => {
+    //   const href = $(link).attr("href");
 
-      if (href && href.startsWith("/url?q=")) {
-        const cleanedHref = href.replace("/url?q=", "").split("&")[0];
+    //   if (href && href.startsWith("/url?q=")) {
+    //     const cleanedHref = href.replace("/url?q=", "").split("&")[0];
 
-        if (!links.includes(cleanedHref)) {
-          links.push(cleanedHref);
-        }
-      }
-    });
+    //     if (!links.includes(cleanedHref)) {
+    //       links.push(cleanedHref);
+    //     }
+    //   }
+    // });
 
-    const filteredLinks = links.filter((link, idx) => {
-      const domain = new URL(link).hostname;
+    // const filteredLinks = links.filter((link, idx) => {
+    //   const domain = new URL(link).hostname;
 
-      const excludeList = [
-        "google",
-        "facebook",
-        "twitter",
-        "instagram",
-        "youtube",
-        "tiktok",
-      ];
-      if (excludeList.some((site) => domain.includes(site))) return false;
+    //   const excludeList = [
+    //     "google",
+    //     "facebook",
+    //     "twitter",
+    //     "instagram",
+    //     "youtube",
+    //     "tiktok",
+    //   ];
+    //   if (excludeList.some((site) => domain.includes(site))) return false;
 
-      return (
-        links.findIndex((link) => new URL(link).hostname === domain) === idx
-      );
-    });
+    //   return (
+    //     links.findIndex((link) => new URL(link).hostname === domain) === idx
+    //   );
+    // });
 
-    const finalLinks = filteredLinks.slice(0, sourceCount);
-    console.log("finalLinks", finalLinks);
-    const sources = (await Promise.all(
-      finalLinks.map(async (link) => {
-        const response = await fetch(link);
-        const html = await response.text();
+    // const finalLinks = filteredLinks.slice(0, sourceCount);
+    // console.log("finalLinks", finalLinks);
+    // const sources = (await Promise.all(
+    //   finalLinks.map(async (link) => {
+    //     const response = await fetch(link);
+    //     const html = await response.text();
 
-        const dom = new JSDOM(html);
-        const doc = dom.window.document;
-        const parsed = new Readability(doc).parse();
+    //     const dom = new JSDOM(html);
+    //     const doc = dom.window.document;
+    //     const parsed = new Readability(doc).parse();
 
-        if (parsed) {
-          let sourceText = cleanSourceText(parsed.textContent);
-          let title = parsed.title;
+    //     if (parsed) {
+    //       let sourceText = cleanSourceText(parsed.textContent);
+    //       let title = parsed.title;
 
-          return { url: link, text: sourceText, title };
-        }
-      })
-    )) as { url: string; text: string }[];
+    //       return { url: link, text: sourceText, title };
+    //     }
+    //   })
+    // )) as { url: string; text: string }[];
 
-    const filteredSources = sources.filter((source) => source !== undefined);
+    // const filteredSources = sources.filter((source) => source !== undefined);
 
-    for (const source of filteredSources) {
-      source.text = source.text.slice(0, 1500);
-    }
+    // for (const source of filteredSources) {
+    //   source.text = source.text.slice(0, 1500);
+    // }
 
-    return NextResponse.json(filteredSources, { status: 201 });
+    return NextResponse.json({'Hello world'}, { status: 201 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 500 });
